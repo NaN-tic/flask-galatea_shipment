cce8503ebf8055f2436955e30ac4e55f609a81ef
resteve <resteve@zikzakmedia.com>
2014-10-06 13:28:54 +0200

    Change str DISPLAY_MSG

--- shipment.py
+++ shipment.py
@@ -7,7 +7,7 @@ from flask.ext.paginate import Pagination
 
 shipment = Blueprint('shipment', __name__, template_folder='templates')
 
-DISPLAY_MSG = lazy_gettext('Displaying <b>{start} - {end}</b> {record_name} of <b>{total}</b>')
+DISPLAY_MSG = lazy_gettext('Displaying <b>{start} - {end}</b> of <b>{total}</b>')
 
 LIMIT = current_app.config.get('TRYTON_PAGINATION_SHIPMENT_LIMIT', 20)
 
